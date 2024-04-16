81601f9a235756b06926d04c49a43a0125f507cb
lucaspbueno <lucaspbueno22@gmail.com>
2024-04-16 20:32:36 -0300

    :recycle: REFACTOR - Altera o estilo padrão da paǵina
    Altera a cor primearia do bootstrap para a #5D3FD3, também altera p background para --bbs-gray-200 e a font para Sora (ainda não instalada) ou a padrão do bootstrap --bs-font-sans-serif

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 function App() {
 
   return (
-    <div className="bg-body-secondary vh-100">
-      <h1 className="">teste</h1>
-      <i className="bi bi-8-circle text-danger h3"></i>
-    </div>
+    <></>
   )
 }
 
-export default App
+export default App;
