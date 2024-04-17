cf7b1f2d6143f0adc478d59150de324482bac2c7
lucaspbueno <lucaspbueno22@gmail.com>
2024-04-16 21:10:20 -0300

    :sparkles: FEAT - Cria a configuração inicial do react-router-dom
    Também cria a rota padrão (rota /) tendo como filho o comoponente Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import Header from "./template/Header.jsx";
+import Footer from "./template/Footer.jsx";
+import Main from "./template/Main.jsx";
+import { Outlet } from 'react-router-dom';
+
 function App() {
 
   return (
-    <></>
+    <>
+      <Header></Header>
+      <Main>
+        <Outlet />
+      </Main>
+      <Footer></Footer>
+    </>
   )
 }
 
